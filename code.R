3457303a2c9399e4f25899cd9d3e7c176fe96b49
BZhang666 <bowen.zhang.18@ucl.ac.uk>
2018-12-31 16:46:33 +0000

    update

--- code.R
+++ code.R
@@ -17,35 +17,14 @@ plot(K)
 
 
 
-
-WastepointSub <- data.frame(WastepointSub@coords[,1:2])
-#now run the dbscan analysis
-db <- fpc::dbscan(WastepointSub, eps = 1500, MinPts = 3)
-#now plot the results
-plot(db, WastepointSub, main = "DBSCAN Output", frame = F)
-plot(BoroughMapBNG, add=T)
-library(ggplot2)
-db
-
-
-
-qtm(LondonWards)
-proj4string(LondonWards) <- CRS("+init=epsg:27700")
-tmap_mode("view")
-WastepointSub2 <- WastepointBNG[LondonWards,]
-tm_shape(LondonWards) +
-  tm_polygons(col = NA, alpha = 0.5) +
-  tm_shape(WastepointSub2) +
-  tm_dots(col = "blue")
-summary(WastepointSub2)
-res <- poly.counts(WastepointSub2, LondonWards)
+res <- poly.counts(Wastepoint, LondonWards)
 LondonWards@data$WastepointCount<-res
-LondonWards@data$BlueDensity <- LondonWards$WastepointCount/poly.areas(LondonWards)
+LondonWards@data$WasteDensity <- LondonWards$WastepointCount/poly.areas(LondonWards)
 LondonWards@data
 
 
 tm_shape(LondonWards) +
-  tm_polygons("BlueDensity",
+  tm_polygons("WasteDensity",
               style="jenks",
               palette="PuOr",
               midpoint=NA,
@@ -55,23 +34,12 @@ LondonWards@data$WasteDensity <- as.numeric()
 library(spdep)
 #####
 #First calculate the centroids of all Wards in London
-coordsW <- coordinates(WardsOUT)
-plot(coordsW)
-LB_nb <- poly2nb(WardsOUT, queen=T)
-#plot them
-plot(LB_nb, coordinates(WardsOUT), col="red")
-#add a map underneath
-plot(WardsOUT, add=T)
-Lward.lw <- nb2listw(LB_nb, style="C")
-head(Lward.lw$neighbours)
-I_LWard_Global_Density <- moran.test(WardsOUT@data$WasteDensity, Lward.lw)
-I_LWard_Global_Density
 
-C_LWard_Global_Density <- geary.test(WardsOUT@data$WasteDensity, Lward.lw)
+
+C_LWard_Global_Density <- geary.test(LondonWardsBNG@data$WasteDensity, Lward.lw)
 C_LWard_Global_Density
 
-G_LWard_Global_Density <- globalG.test(WardsOUT@data$WasteDensity, Lward.lw)
-G_LWard_Global_Density
+
 
 
 LondonWardsSF <- st_as_sf(LondonWards)
